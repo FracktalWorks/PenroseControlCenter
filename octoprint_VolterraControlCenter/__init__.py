5d53e7d36ebfe5e3a32468bd64814e816297e527
Vijay Raghav Varada <vjvarada@gmail.com>
2025-12-23 14:56:45 +0530

    changes to Volterra Control Center

--- octoprint_VolterraControlCenter/__init__.py
+++ octoprint_VolterraControlCenter/__init__.py
@@ -41,7 +41,7 @@ del get_versions
 # 			self.is_running = False
 
 
-class ControlCenter(octoprint.plugin.StartupPlugin):
+class VolterraControlCenter(octoprint.plugin.StartupPlugin):
     def on_after_startup(self):
         # self.resetInetrval = int(self._settings.get(["resetInetrval"]))
         self._logger.info("TouchUI Plugin Started")
@@ -88,7 +88,7 @@ sudo python3 main.py
                     current_content = f.read()
                     
                 # Check if the content contains the correct path to our plugin
-                if plugin_dir not in current_content or "octoprint_ControlCenter" not in current_content:
+                if plugin_dir not in current_content or "octoprint_VolterraControlCenter" not in current_content:
                     needs_update = True
                     self._logger.info(f"xinit file does not contain correct path to {plugin_dir}")
                 else:
@@ -211,28 +211,28 @@ sudo python3 main.py
     def get_update_information(self):
         return dict(
             ControlCenter=dict(
-                displayName="ControlCenter",
+                displayName="VolterraControlCenter",
                 displayVersion=self._plugin_version,
                 # version check: github repository
                 type="github_release",
                 user="FracktalWorks",
-                repo="ControlCenter",
+                repo="VolterraControlCenter",
                 current=self._plugin_version,
 
                 # update method: pip
-                pip="https://github.com/FracktalWorks/ControlCenter/archive/{target_version}.zip"
+                pip="https://github.com/FracktalWorks/VolterraControlCenter/archive/{target_version}.zip"
             )
         )
 
 
-__plugin_name__ = "ControlCenter"
+__plugin_name__ = "VolterraControlCenter"
 __plugin_version__ = __version__
 __plugin_pythoncompat__ = ">=3,<4"
 
 
 def __plugin_load__():
     global __plugin_implementation__
-    __plugin_implementation__ = ControlCenter()
+    __plugin_implementation__ = VolterraControlCenter()
 
     global __plugin_hooks__
     __plugin_hooks__ = {
